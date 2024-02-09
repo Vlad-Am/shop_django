a0b34b67e1dd2e6dd8b59e7037d6ecb6a4d38a7f
vlad Amirov <vladamirov@list.ru>
2024-02-09 18:30:58 +0300

    автоматическое заполнение БД

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 from catalog.models import Categories, Products
 
+
 # Register your models here.
 
-admin.site.register(Products)
-admin.site.register(Categories)
+# admin.site.register(Products)
+# admin.site.register(Categories)
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price")
+    list_filter = ("category", "price",)
+    search_fields = ("name", "description",)
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("id", "category", "description")
+    list_filter = ("category", "id",)
+    search_fields = ("category", "description",)
